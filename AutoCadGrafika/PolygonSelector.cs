15003a9879fcb50b34d59bdbed4703c43eee2e15
Nenad Dzeverdanovic <nenad.dzeverdanovic@easygoing.company>
2019-12-19 16:13:07 +0100

    pocetak

---
+++ AutoCadGrafika/PolygonSelector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace AutoCadGrafika
+{
+    public  class Commands
+    {
+        [CommandMethod("SelectObjectsByPolygon")]
+        public  void SelectObjectsByCrossingWindow()
+        {
+            // Get the current document editor
+            Editor acDocEd = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            // Create a crossing window from (2,2,0) to (10,8,0)
+            PromptSelectionResult acSSPrompt;
+            acSSPrompt = acDocEd.SelectCrossingWindow(new Point3d(2, 2, 0),
+                                                        new Point3d(10, 8, 0));
+            //acSSPrompt=acDocEd.SelectCrossingPolygon(()
+
+            // If the prompt status is OK, objects were selected
+            if (acSSPrompt.Status == PromptStatus.OK)
+            {
+                SelectionSet acSSet = acSSPrompt.Value;
+
+                Application.ShowAlertDialog("Number of objects selected: " +
+                                            acSSet.Count.ToString());
+            }
+            else
+            {
+                Application.ShowAlertDialog("Number of objects selected: 0");
+            }
+        }
+
+    }
+}
