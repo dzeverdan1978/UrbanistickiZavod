89d700bb0ec1e4ea2ae63caabaf13258157b6598
Nenad Dzeverdanovic <nenad.dzeverdanovic@easygoing.company>
2020-01-16 13:44:23 +0100

    Izmenjena kompletna logika

--- AutoCadGrafika/PolygonSelector.cs
+++ AutoCadGrafika/PolygonSelector.cs
@@ -10,140 +10,94 @@ using Autodesk.AutoCAD.EditorInput;
 using System.ComponentModel;
 using System.Windows.Forms;
 
+using MgdAcApplication = Autodesk.AutoCAD.ApplicationServices.Application;
+using MgdAcDocument = Autodesk.AutoCAD.ApplicationServices.Document;
+using AcWindowsNS = Autodesk.AutoCAD.Windows;
+
 namespace AutoCadGrafika
 {
     public  class Commands
     {
-        [CommandMethod("SelectObjectsByPolygon")]
-        public  void SelectObjectsByCrossingWindow()
+        [CommandMethod("SelectObjectsByPolygon",CommandFlags.Modal | CommandFlags.Redraw)]
+        public static  void SelectObjectsByCrossingWindow()
         {
             string status = "";
+            Editor ed = MgdAcApplication.DocumentManager.MdiActiveDocument.Editor;
+            var acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            status = "Pocetak";
+
             try
             {
-                // Get the current document editor
-                Editor acDocEd = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
-                var acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
-                Database acCurDb = acDoc.Database;
-
-                status = "Pocetak";
-
-                // Selektuj poligon za izdvajanje planova
-                PromptSelectionResult acSSPrompt = acDoc.Editor.GetSelection();
-
-                // If the prompt status is OK, objects were selected
-                if (acSSPrompt.Status == PromptStatus.OK)
+                using (PointCollector2 pc = new PointCollector2(PointCollector2.Shape.Polygon, true))
                 {
-                    SelectionSet acSSet = acSSPrompt.Value;
-
-                    status = "Vracen Selection Set";
-
-                    if (acSSet.Count > 1)
-                        Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Selektovano je vise objekata. Selektujte samo jedan! ");
-                    else
-                    {
-                        // Vrati skup tacaka za poligon
-                        var selPolygon = acSSet[0];
-
-                        status = "Vracen poligon iz SelectionSet";
-
-                        if (selPolygon != null)
+                    using (SelectionSet ss = pc.Select())
+                        if (ss != null && ss.Count > 0)
                         {
+                            ed.SetImpliedSelection(ss.GetObjectIds().ToArray());
+                            List<string> karts = new List<string>();
+
                             using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
                             {
-                                BlockTableRecord btr = (BlockTableRecord)acTrans.GetObject(
-                                    acCurDb.CurrentSpaceId, OpenMode.ForRead);
-
-                                status = "Vracen BTR iz baze";
-
-
-                                Entity acEnt = acTrans.GetObject(selPolygon.ObjectId,
-                                                            OpenMode.ForRead) as Entity;
-                                status = "Vracen entitet iz baze za poligon";
-
-                                if (acEnt != null)
+                                foreach (SelectedObject plan in ss)
                                 {
+                                    // procitaj atribut KARTBROJ i dodaj ga u rezultat
+                                    Entity planEnt = acTrans.GetObject(plan.ObjectId,
+                                                    OpenMode.ForRead) as Entity;
+                                    status = "Vracen entitet za pojedinacni plan";
 
-                                    Point3dCollection entPts = CollectPoints(acTrans, acEnt);
-                                    status = "Vracene tacke za poligon";
+                                    object acadObj = planEnt.AcadObject;
+                                    status = "Vracen ACAD objekat za plan";
+                                    var props = TypeDescriptor.GetProperties(acadObj);
+                                    status = "Vraceni properties za ACAD object";
 
-                                    if (entPts != null && entPts.Count > 0)
+                                    foreach (PropertyDescriptor prop in props)
                                     {
-                                        // Selektuj planove na osnovu poligona
-                                        PromptSelectionResult selPlanovi = acDoc.Editor.SelectCrossingPolygon(entPts);
-                                        if (selPlanovi.Status==PromptStatus.OK)
-                                            status = "Vraceni selektovani planovi pomocu poligona";
-                                        else
-                                            status = "Nije selektovan ni jedan plan pomocu poligona";
-
-                                        List<string> karts = new List<string>();
-
-                                        foreach (SelectedObject plan in selPlanovi.Value)
+                                        if (prop.DisplayName.ToLower().Contains("kart"))
                                         {
-                                            // procitaj atribut KARTBROJ i dodaj ga u rezultat
-                                            Entity planEnt = acTrans.GetObject(plan.ObjectId,
-                                                            OpenMode.ForRead) as Entity;
-                                            status = "Vracen entitet za pojedinacni plan";
-
-                                            object acadObj = planEnt.AcadObject;
-                                            status = "Vracen ACAD objekat za plan";
-                                            var props = TypeDescriptor.GetProperties(acadObj);
-                                            status = "Vraceni properties za ACAD object";
-
-                                            foreach (PropertyDescriptor prop in props)
+                                            object value = prop.GetValue(acadObj);
+                                            if (value != null)
                                             {
-                                                if (prop.DisplayName.ToLower().Contains("kart"))
+                                                if (value.ToString().Contains(","))
                                                 {
-                                                    object value = prop.GetValue(acadObj);
-                                                    if (value != null)
-                                                    {
-                                                        if (value.ToString().Contains(","))
-                                                        {
-                                                            string[] kbr = value.ToString().Split(',');
-                                                            foreach (string kb in kbr)
-                                                                if (!karts.Contains(kb))
-                                                                    karts.Add(kb);
-                                                        }
-                                                        else
-                                                        {
-                                                            if (!karts.Contains(value.ToString()))
-                                                                karts.Add(value.ToString());
-                                                        }
-                                                    }
-                                                        
+                                                    string[] kbr = value.ToString().Split(',');
+                                                    foreach (string kb in kbr)
+                                                        if (!karts.Contains(kb))
+                                                            karts.Add(kb);
                                                 }
-                                                status = "Vracen propery KART";
+                                                else
+                                                {
+                                                    if (!karts.Contains(value.ToString()))
+                                                        karts.Add(value.ToString());
+                                                }
                                             }
-                                        }
-                                        string ispis = "";
-                                        foreach (string kb in karts)
-                                            ispis += kb + ",";
-                                        if (ispis.EndsWith(","))
-                                            ispis = ispis.Substring(0, ispis.Length - 1);
-                                        if (ispis.Length > 0)
-                                        {
-                                            Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Izdvojeni su sledeci katografski brojevi\n"
-                                                + ispis + "\nKartografski brojevi su spremni za koriscenje u aplikaciji");
-
-                                            Clipboard.SetText(ispis);
-                                            status = "Rezultat dodat u Clipboard";
 
                                         }
-                                        else
-                                            Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Nema ni jednog kartografskog broja. Pokusajte ponovo");
-
+                                        status = "Vracen propery KART";
                                     }
                                 }
-
                                 acTrans.Commit();
                             }
+                            string ispis = "";
+                            foreach (string kb in karts)
+                                ispis += kb + ",";
+                            if (ispis.EndsWith(","))
+                                ispis = ispis.Substring(0, ispis.Length - 1);
+                            if (ispis.Length > 0)
+                            {
+                                Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Izdvojeni su sledeci katografski brojevi\n"
+                                    + ispis + "\nKartografski brojevi su spremni za koriscenje u aplikaciji");
+
+                                Clipboard.SetText(ispis);
+                                status = "Rezultat dodat u Clipboard";
+
+                            }
+                            else
+                                Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Nema ni jednog kartografskog broja. Pokusajte ponovo");
+
                         }
-                    }
                 }
-                else
-                {
-                    Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("Nista nije elektovano. Molim selektujte poligon za pretragu!");
-                }
-                status = "Sve proslo OK";
+                
             }
             catch (System.Exception ex)
             {
@@ -156,429 +110,7 @@ namespace AutoCadGrafika
         
         }
 
-        private Point3dCollection CollectPoints(Transaction tr, Entity ent)
-
-        {
-
-            // The collection of points to populate and return
-
-
-
-            Point3dCollection pts = new Point3dCollection();
-
-
-
-            // We'll start by checking a block reference for
-
-            // attributes, getting their bounds and adding
-
-            // them to the point list. We'll still explode
-
-            // the BlockReference later, to gather points
-
-            // from other geometry, it's just that approach
-
-            // doesn't work for attributes (we only get the
-
-            // AttributeDefinitions, which don't have bounds)
-
-
-
-            BlockReference br = ent as BlockReference;
-
-            if (br != null)
-
-            {
-
-                foreach (ObjectId arId in br.AttributeCollection)
-
-                {
-
-                    DBObject obj = tr.GetObject(arId, OpenMode.ForRead);
-
-                    if (obj is AttributeReference)
-
-                    {
-
-                        AttributeReference ar = (AttributeReference)obj;
-
-                        ExtractBounds(ar, pts);
-
-                    }
-
-                }
-
-            }
-
-
-
-            // If we have a curve - other than a polyline, which
-
-            // we will want to explode - we'll get points along
-
-            // its length
-
-
-
-            Curve cur = ent as Curve;
-
-            if (cur != null &&
-
-                !(cur is Polyline ||
-
-                  cur is Polyline2d ||
-
-                  cur is Polyline3d))
-
-            {
-
-                // Two points are enough for a line, we'll go with
-
-                // a higher number for other curves
-
-
-
-                int segs = (ent is Line ? 2 : 20);
-
-
-
-                double param = cur.EndParam - cur.StartParam;
-
-                for (int i = 0; i < segs; i++)
-
-                {
-
-                    try
-
-                    {
-
-                        Point3d pt =
-
-                          cur.GetPointAtParameter(
-
-                            cur.StartParam + (i * param / (segs - 1))
-
-                          );
-
-                        pts.Add(pt);
-
-                    }
-
-                    catch { }
-
-                }
-
-            }
-
-            else if (ent is DBPoint)
-
-            {
-
-                // Points are easy
-
-
-
-                pts.Add(((DBPoint)ent).Position);
-
-            }
-
-            else if (ent is DBText)
-
-            {
-
-                // For DBText we use the same approach as
-
-                // for AttributeReferences
-
-
-
-                ExtractBounds((DBText)ent, pts);
-
-            }
-
-            else if (ent is MText)
-
-            {
-
-                // MText is also easy - you get all four corners
-
-                // returned by a function. That said, the points
-
-                // are of the MText's box, so may well be different
-
-                // from the bounds of the actual contents
-
-
-
-                MText txt = (MText)ent;
-
-                Point3dCollection pts2 = txt.GetBoundingPoints();
-
-                foreach (Point3d pt in pts2)
-
-                {
-
-                    pts.Add(pt);
-
-                }
-
-            }
-
-            else if (ent is Face)
-
-            {
-
-                Face f = (Face)ent;
-
-                try
-
-                {
-
-                    for (short i = 0; i < 4; i++)
-
-                    {
-
-                        pts.Add(f.GetVertexAt(i));
-
-                    }
-
-                }
-
-                catch { }
-
-            }
-
-            else if (ent is Solid)
-
-            {
-
-                Solid sol = (Solid)ent;
-
-                try
-
-                {
-
-                    for (short i = 0; i < 4; i++)
-
-                    {
-
-                        pts.Add(sol.GetPointAt(i));
-
-                    }
-
-                }
-
-                catch { }
-
-            }
-
-            else
-
-            {
-
-                // Here's where we attempt to explode other types
-
-                // of object
-
-
-
-                DBObjectCollection oc = new DBObjectCollection();
-
-                try
-
-                {
-
-                    ent.Explode(oc);
-
-                    if (oc.Count > 0)
-
-                    {
-
-                        foreach (DBObject obj in oc)
-
-                        {
-
-                            Entity ent2 = obj as Entity;
-
-                            if (ent2 != null && ent2.Visible)
-
-                            {
-
-                                foreach (Point3d pt in CollectPoints(tr, ent2))
-
-                                {
-
-                                    pts.Add(pt);
-
-                                }
-
-                            }
-
-                            obj.Dispose();
-
-                        }
-
-                    }
-
-                }
-
-                catch { }
-
-            }
-
-            return pts;
-
-        }
-
-
-
-        private void ExtractBounds(DBText txt, Point3dCollection pts)
-
-        {
-
-            // We have a special approach for DBText and
-
-            // AttributeReference objects, as we want to get
-
-            // all four corners of the bounding box, even
-
-            // when the text or the containing block reference
-
-            // is rotated
-
-
-
-            if (txt.Bounds.HasValue && txt.Visible)
-
-            {
-
-                // Create a straight version of the text object
-
-                // and copy across all the relevant properties
-
-                // (stopped copying AlignmentPoint, as it would
-
-                // sometimes cause an eNotApplicable error)
-
-
-
-                // We'll create the text at the WCS origin
-
-                // with no rotation, so it's easier to use its
-
-                // extents
-
-
-
-                DBText txt2 = new DBText();
-
-                txt2.Normal = Vector3d.ZAxis;
-
-                txt2.Position = Point3d.Origin;
-
-
-
-                // Other properties are copied from the original
-
-
-
-                txt2.TextString = txt.TextString;
-
-                txt2.TextStyleId = txt.TextStyleId;
-
-                txt2.LineWeight = txt.LineWeight;
-
-                txt2.Thickness = txt2.Thickness;
-
-                txt2.HorizontalMode = txt.HorizontalMode;
-
-                txt2.VerticalMode = txt.VerticalMode;
-
-                txt2.WidthFactor = txt.WidthFactor;
-
-                txt2.Height = txt.Height;
-
-                txt2.IsMirroredInX = txt2.IsMirroredInX;
-
-                txt2.IsMirroredInY = txt2.IsMirroredInY;
-
-                txt2.Oblique = txt.Oblique;
-
-
-
-                // Get its bounds if it has them defined
-
-                // (which it should, as the original did)
-
-
-
-                if (txt2.Bounds.HasValue)
-
-                {
-
-                    Point3d maxPt = txt2.Bounds.Value.MaxPoint;
-
-
-
-                    // Place all four corners of the bounding box
-
-                    // in an array
-
-
-
-                    Point2d[] bounds =
-
-                      new Point2d[] {
-
-              Point2d.Origin,
-
-              new Point2d(0.0, maxPt.Y),
-
-              new Point2d(maxPt.X, maxPt.Y),
-
-              new Point2d(maxPt.X, 0.0)
-
-                      };
-
-
-
-                    // We're going to get each point's WCS coordinates
-
-                    // using the plane the text is on
-
-
-
-                    Plane pl = new Plane(txt.Position, txt.Normal);
-
-
-
-                    // Rotate each point and add its WCS location to the
-
-                    // collection
-
-
-
-                    foreach (Point2d pt in bounds)
-
-                    {
-
-                        pts.Add(
-
-                          pl.EvaluatePoint(
-
-                            pt.RotateBy(txt.Rotation, Point2d.Origin)
-
-                          )
-
-                        );
-
-                    }
-
-                }
-
-            }
-
-        }
+        
 
     }
 }
